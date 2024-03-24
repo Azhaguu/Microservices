c7ad3e336a571af68e957ac9bf75753c03b173e0
Azhaguu <jaganjacker@gmail.com>
2024-03-24 23:19:06 +0530

    gateway added

---
+++ APIGateway/src/test/java/com/getway/APIGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.getway.APIGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
